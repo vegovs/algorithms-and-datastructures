6c51d46f12f067630fb233aa2041c31a09a32926
Vegard Bergsvik Øvstegård <vegovs@gmail.com>
2018-10-09 21:37:27 +0200

    Made BST more general(Key extends Comparable<Key>)

--- RedBlackTree.java
+++ RedBlackTree.java
@@ -1,17 +1,11 @@
+import java.util.Comparator;
+
 public class RedBlackTree extends BinarySearchTree {
 
-    /**
-     * Constructs an empty RedBlackTree that can only accept Comparables as
-     * items.
-     */
     public RedBlackTree() {
         this(null);
     }
 
-    /**
-     * Constructs an empty RedBlackTree that orders its items according to the
-     * given comparator.
-     */
     public RedBlackTree(Comparator c) {
         super(c);
     }
