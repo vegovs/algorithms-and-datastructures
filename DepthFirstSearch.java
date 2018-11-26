20754dd6c2d551f2f01940be989615208b6bd464
Vegard Bergsvik Øvstegård <vegovs@gmail.com>
2018-11-26 22:07:06 +0100

    :bug: Fixed typo

--- DepthFirstSearch.java
+++ DepthFirstSearch.java
@@ -47,7 +47,7 @@ class Graph{
 		g.addEdge(2, 3); 
 		g.addEdge(3, 3);
 
-		System.out.println("Following is Breadth First Traversal "+ 
+		System.out.println("Following is Depth First Traversal "+ 
 										   "(starting from vertex 2)");
 
 	g.depthFirstSearch(2);
